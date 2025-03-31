f1a81f447a6e2d87f2c9f772aee048fa5db7ccf2
Devin Frazier <Dev.fraz18@gmail.com>
2025-03-30 21:30:57 -0400

    got activities to render on activity page

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,8 +1,21 @@
+import useQuery from "../api/useQuery";
+
 export default function ActivitiesPage() {
+  const { data: activities, loading, error } = useQuery("/activities");
+
+  if (loading) return <p>Loading activities...</p>;
+  if (error) return <p>Error: {error}</p>;
   return (
     <>
       <h1>Activities</h1>
       <p>Imagine all the activities!</p>
+      <ul id="activities">
+        {activities.map((activity) => (
+          <li key={activity.id}>
+            <p>{activity.name}</p>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
