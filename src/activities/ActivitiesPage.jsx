7b16cdd75f543394ec8e12544e35785b6090ad43
Devin Frazier <Dev.fraz18@gmail.com>
2025-04-01 13:13:23 -0400

    added delete button and form

--- src/activities/ActivitiesPage.jsx
+++ src/activities/ActivitiesPage.jsx
@@ -1,13 +1,15 @@
-import useQuery from "../api/useQuery";
+/*import useQuery from "../api/useQuery";
+import { useAuth } from "../auth/AuthContext";
+import useMutation from "../api/useMutation";*/
 
-export default function ActivitiesPage() {
+/*export default function ActivitiesPage() {
   const {
     data: activities,
     loading,
     error,
-  } = useQuery("/activities", ["activitylist"]);
+  } = useQuery("/activities", "activitylist");
 
-  if (loading) return <p>Loading activities...</p>;
+  /*if (loading) return <p>Loading activities...</p>;
   if (error) return <p>Error: {error}</p>;
   return (
     <>
@@ -20,6 +22,54 @@ export default function ActivitiesPage() {
           </li>
         ))}
       </ul>
+    </>
+  );
+
+  if (loading || !activities) return <p>Loading...</p>;
+  if (error) return <p>Sorry! {error}</p>;
+
+  return (
+    <ul>
+      {activities.map((activity) => (
+        <ActivityListItem key={activity.id} activity={activity} />
+      ))}
+    </ul>
+  );
+}
+
+function ActivityListItem({ activity }) {
+  const { token } = useAuth();
+  const {
+    mutate: deleteActivity,
+    loading,
+    error,
+  } = useMutation("DELETE", "/activities/" + activity.id, ["activities"]);
+
+  return (
+    <li>
+      <p>{activity.name}</p>
+      {token && (
+        <button onClick={() => deleteActivity()}>
+          {loading ? "Deleting" : error ? error : "Delete"}
+        </button>
+      )}
+    </li>
+  );
+}*/
+
+import { useAuth } from "../auth/AuthContext";
+
+import ActivityList from "./ActivityList";
+import ActivityForm from "./ActivityForm";
+
+export default function ActivitiesPage() {
+  const { token } = useAuth();
+
+  return (
+    <>
+      <h1>Activities</h1>
+      <ActivityList />
+      {token && <ActivityForm />}
     </>
   );
 }
